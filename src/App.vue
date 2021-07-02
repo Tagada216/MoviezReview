f30d026da0b111f7ea7b1dfaa33d12953f18082f
Nicolas Delcourt <nicolasdelcourt10@gmail.com>
2021-07-02 09:54:09 +0200

    test deploy

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 <div>
   <header>
     <div class="header-bar">
-      <p class="title">The Moviez Review</p>
+      <p class="title">The Moviez </p>
       <div class="btn-container">
         <button class="btn-1"> <router-link to='/'>Accueil </router-link>  </button>
         <button class="btn-1"> <router-link to='/list'>Ma Liste</router-link>  </button>
